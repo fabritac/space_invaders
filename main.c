16084ddc862c762fe270862a9a91a547494d6a2b
Fabri <fabritacchi@gmail.com>
2025-07-21 19:51:36 -0300

    Refactor code

--- main.c
+++ main.c
@@ -4,12 +4,10 @@
 #include <stdlib.h>
 
 #define MAX_ALIENS 10
-#define MAX_BULLETS 5 
+#define MAX_BULLETS 5
 
 #define SCREEN_WIDTH 500
 #define SCREEN_HEIGHT 500
-
-// TODO: Separar las bullets del jugador con las bullets de los aliens
 
 typedef enum {
     PLAYER,
@@ -28,6 +26,22 @@ typedef struct {
     int timer;
 } Entity;
 
+void update_player(Entity *player, Entity *player_bullets, int* player_bullet_count);
+
+void draw_player(Entity *player);
+
+void draw_player_bullets(Entity *player_bullets, int *player_bullet_count);
+
+void draw_aliens(Entity *aliens, int *alive_alien_count);
+
+void update_aliens(Entity* aliens, int* alive_alien_count, Entity* alien_bullets, int* alien_bullet_count);
+
+void update_alien_bullets(Entity *alien_bullets, int *alien_bullet_count);
+
+void draw_alien_bullets(Entity *alien_bullets, int *alien_bullet_count);
+
+void update_player_bullets(Entity *player_bullets, int *player_bullet_count, Entity *alive_aliens, int *alive_alien_count, Entity *dead_aliens, int *dead_alien_count);
+
 int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Invaders");
 
@@ -36,7 +50,7 @@ int main(void) {
         .kind = PLAYER,
         .pos = (Vector2){250, 250},
         .vel = (Vector2){0, 0},
-        .shape = (Rectangle){250, 250, 50, 50}
+        .shape = (Rectangle){250, 250, 10, 10}
     };
 
     // Setup aliens
@@ -57,10 +71,11 @@ int main(void) {
     }
 
     // Setup bullets
-    Entity alive_bullets[MAX_BULLETS];
-    Entity dead_bullets[256]; // <-- Buscar manera de reutilizar este array, temporal el 256
-    int alive_bullet_count = 0;
-    int dead_bullet_count = 0;
+    Entity player_bullets[MAX_BULLETS];
+    Entity alien_bullets[MAX_BULLETS];
+//    Entity dead_bullets[256]; // <-- Buscar manera de reutilizar este array, temporal el 256
+    int player_bullet_count = 0;
+    int alien_bullet_count = 0;
 
     SetTargetFPS(60);
 
@@ -69,129 +84,152 @@ int main(void) {
 
         ClearBackground(WHITE);
 
-        // --- Update things
+        update_player(&player, player_bullets, &player_bullet_count);
 
-        // Update player
+        update_player_bullets(player_bullets, &player_bullet_count, alive_aliens, &alive_alien_count, dead_aliens, &dead_alien_count);
 
-        if(IsKeyDown(KEY_RIGHT)) player.vel.x += 1;
-        if(IsKeyDown(KEY_LEFT)) player.vel.x -= 1;
-        if(IsKeyDown(KEY_UP)) player.vel.y -= 1;
-        if(IsKeyDown(KEY_DOWN)) player.vel.y += 1;
+        update_aliens(alive_aliens, &alive_alien_count, alien_bullets, &alien_bullet_count);
 
-        player.pos.x += player.vel.x;
-        player.pos.y += player.vel.y;
+        update_alien_bullets(alien_bullets, &alien_bullet_count);
 
-        if (player.pos.x < 0) {
-            player.pos.x = 0;
-            player.vel.x = 0;
-        } else if (player.pos.x + player.shape.width > SCREEN_WIDTH) {
-            player.pos.x = SCREEN_WIDTH - player.shape.width;
-            player.vel.x = 0;
-        }
 
-        if (player.pos.y < 0) {
-            player.pos.y = 0;
-            player.vel.y = 0;
-        } else if (player.pos.y + player.shape.height > SCREEN_HEIGHT) {
-            player.pos.y = SCREEN_HEIGHT - player.shape.height;
-            player.vel.y = 0;
-        }
+        draw_player(&player);
+        draw_player_bullets(player_bullets, &player_bullet_count);
 
-        player.shape = (Rectangle){player.pos.x, player.pos.y, 50, 50};
-
-        // Update aliens
-        for (int i = 0; i < alive_alien_count; i++) {
-            alive_aliens[i].pos.x += alive_aliens[i].vel.x;
-
-            if (alive_aliens[i].pos.x < 0) {
-                alive_aliens[i].pos.x = 0;
-                alive_aliens[i].vel.x *= -1;
-            } else if (alive_aliens[i].pos.x + alive_aliens[i].shape.width > SCREEN_WIDTH) {
-                alive_aliens[i].vel.x *= -1;
-            }
-            alive_aliens[i].shape = (Rectangle){alive_aliens[i].pos.x, alive_aliens[i].pos.y, 10, 10};
-            
-            // Alien bullet logic
-            if (alive_aliens[i].timer < 0 && alive_bullet_count < MAX_BULLETS) {
-                alive_bullets[alive_bullet_count] = (Entity) {
-                    .kind = BULLET,
-                    .pos = (Vector2){alive_aliens[i].pos.x, alive_aliens[i].pos.y},
-                    .vel = (Vector2){0, 5},
-                    .shape = (Rectangle){alive_aliens[i].pos.x, alive_aliens[i].pos.y, 10, 10}
-                };
-                alive_bullet_count++;
-                alive_aliens[i].timer = 3;
-            }
-            alive_aliens[i].timer--;
-
-        }
-
-        // Player bullet logic
-        if (IsKeyPressed(KEY_SPACE) && alive_bullet_count < MAX_BULLETS) {
-            alive_bullets[alive_bullet_count] = (Entity) {
-                .kind = BULLET,
-                .pos = (Vector2){player.pos.x, player.pos.y},
-                .vel = (Vector2){0, -5},
-                .shape = (Rectangle){player.pos.x, player.pos.y, 10, 10}
-            };
-            alive_bullet_count++;
-        }
-
-        // Update position of the bullets
-        for (int i = 0; i < alive_bullet_count; i++) {
-            alive_bullets[i].pos.y += alive_bullets[i].vel.y;
-            alive_bullets[i].shape.y = alive_bullets[i].pos.y;
-        }
-
-        // Check collision of the bullets with the aliens
-        for (int i = 0; i < alive_bullet_count; i++) {
-            for (int j = 0; j < alive_alien_count; j++) {
-                bool collision = CheckCollisionRecs(alive_bullets[i].shape, alive_aliens[j].shape);
-                if (collision) {
-                    dead_aliens[dead_alien_count] = alive_aliens[j];
-                    dead_alien_count++;
-
-                    for (int k = j; k < alive_alien_count; k++) {
-                        alive_aliens[k] = alive_aliens[k + 1];
-                    }
-                    alive_alien_count--;
-                    j--;
-                }
-            }
-        }
-
-        // Eliminate bullets outside screen
-        for (int i = 0; i < alive_bullet_count; i++) {
-            if (alive_bullets[i].pos.y + alive_bullets[i].shape.height < 0 ||
-                    alive_bullets[i].pos.y + alive_bullets[i].shape.height > SCREEN_HEIGHT) {
-                dead_bullets[dead_bullet_count] = alive_bullets[i];
-                dead_bullet_count++;
-
-                for (int j = i; j < alive_bullet_count - 1; j++) {
-                    alive_bullets[j] = alive_bullets[j + 1];
-                }
-                alive_bullet_count--;
-                i--;
-            }
-        }
-
-        // --- Draw things
-
-        // Draw player
-        DrawRectangleRec(player.shape, RED);
-
-        // Draw aliens
-        for (int i = 0; i < alive_alien_count; i++) {
-            DrawRectangleRec(alive_aliens[i].shape, GREEN);
-        }
-
-        // Draw bullets
-        for (int i = 0; i < alive_bullet_count; i++) {
-            DrawRectangleRec(alive_bullets[i].shape, BLUE);
-        }
+        draw_aliens(alive_aliens, &alive_alien_count);
+        draw_alien_bullets(alien_bullets, &alien_bullet_count);
 
         EndDrawing();
     }
     CloseWindow();
     return 0;
+}
+
+void update_player(Entity *player, Entity *player_bullets, int* player_bullet_count) {
+  if(IsKeyDown(KEY_RIGHT)) player->vel.x += 1;
+  if(IsKeyDown(KEY_LEFT)) player->vel.x -= 1;
+  if(IsKeyDown(KEY_UP)) player->vel.y -= 1;
+  if(IsKeyDown(KEY_DOWN)) player->vel.y += 1;
+
+  if (IsKeyPressed(KEY_SPACE) && *player_bullet_count < MAX_BULLETS) {
+      player_bullets[*player_bullet_count].kind = BULLET;
+      player_bullets[*player_bullet_count].pos = (Vector2){player->pos.x, player->pos.y};
+      player_bullets[*player_bullet_count].vel = (Vector2){0, -5};
+      player_bullets[*player_bullet_count].shape = (Rectangle){player->pos.x, player->pos.y, 10, 10};
+      (*player_bullet_count)++;
+  }
+
+  player->pos.x += player->vel.x;
+  player->pos.y += player->vel.y;
+
+  if (player->pos.x < 0) {
+    player->pos.x = 0;
+    player->vel.x = 0;
+  } else if (player->pos.x + player->shape.width > SCREEN_WIDTH) {
+    player->pos.x = SCREEN_WIDTH - player->shape.width;
+    player->vel.x = 0;
+  }
+
+  if (player->pos.y < 0) {
+    player->pos.y = 0;
+    player->vel.y = 0;
+  } else if (player->pos.y + player->shape.height > SCREEN_HEIGHT) {
+    player->pos.y = SCREEN_HEIGHT - player->shape.height;
+    player->vel.y = 0;
+  }
+
+  player->shape = (Rectangle){player->pos.x, player->pos.y, 10, 10};
+}
+
+void update_aliens(Entity* alive_aliens, int* alive_alien_count,
+                   Entity* alien_bullets, int* alien_bullet_count) {
+  for (int i = 0; i < *alive_alien_count; i++) {
+    alive_aliens[i].pos.x += alive_aliens[i].vel.x;
+
+    if (alive_aliens[i].pos.x < 0) {
+      alive_aliens[i].pos.x = 0;
+      alive_aliens[i].vel.x *= -1;
+    } else if (alive_aliens[i].pos.x + alive_aliens[i].shape.width > SCREEN_WIDTH) {
+      alive_aliens[i].vel.x *= -1;
+    }
+    alive_aliens[i].shape = (Rectangle){alive_aliens[i].pos.x, alive_aliens[i].pos.y, 10, 10};
+
+    if (alive_aliens[i].timer < 0 && *alien_bullet_count < MAX_BULLETS) {
+      alien_bullets[*alien_bullet_count].kind = BULLET;
+      alien_bullets[*alien_bullet_count].pos = (Vector2){alive_aliens[i].pos.x, alive_aliens[i].pos.y};
+      alien_bullets[*alien_bullet_count].vel = (Vector2){0, 5};
+      alien_bullets[*alien_bullet_count].shape = (Rectangle){alive_aliens[i].pos.x, alive_aliens[i].pos.y, 10, 10};
+      (*alien_bullet_count)++;
+      alive_aliens[i].timer = 3;
+    }
+    alive_aliens[i].timer--;
+  }
+}
+
+void update_alien_bullets(Entity *alien_bullets, int *alien_bullet_count) {
+  for (int i = 0; i < *alien_bullet_count; i++) {
+    alien_bullets[i].pos.y += alien_bullets[i].vel.y;
+    alien_bullets[i].shape.y = alien_bullets[i].pos.y;
+
+    if (alien_bullets[i].pos.y + alien_bullets[i].shape.height > SCREEN_HEIGHT) {
+      for (int j = i; j < *alien_bullet_count - 1; j++) {
+        alien_bullets[j] = alien_bullets[j + 1];
+      }
+      (*alien_bullet_count)--;
+      i--;
+    }
+  }
+}
+
+void draw_alien_bullets(Entity *alien_bullets, int *alien_bullet_count) {
+  for (int i = 0; i < *alien_bullet_count; i++) {
+    DrawRectangleRec(alien_bullets[i].shape, BLUE);
+  }
+}
+
+void update_player_bullets(Entity *player_bullets, int *player_bullet_count,
+                           Entity *alive_aliens, int *alive_alien_count,
+                           Entity *dead_aliens, int *dead_alien_count) {
+  for (int i = 0; i < *player_bullet_count; i++) {
+    player_bullets[i].pos.y += player_bullets[i].vel.y;
+    player_bullets[i].shape.y = player_bullets[i].pos.y;
+
+    if (player_bullets[i].pos.y + player_bullets[i].shape.height < 0) {
+      for (int j = i; j < *player_bullet_count - 1; j++) {
+        player_bullets[j] = player_bullets[j + 1];
+      }
+      (*player_bullet_count)--;
+      i--;
+    }
+
+    for (int j = 0; j < *alive_alien_count; j++) {
+      bool collision = CheckCollisionRecs(player_bullets[i].shape, alive_aliens[j].shape);
+      if (collision) {
+        dead_aliens[*dead_alien_count] = alive_aliens[j];
+        (*dead_alien_count)++;
+
+        for (int k = j; k < *alive_alien_count; k++) {
+          alive_aliens[k] = alive_aliens[k + 1];
+        }
+        (*alive_alien_count)--;
+        j--;
+      }
+    }
+  }
+}
+
+void draw_player(Entity *player) {
+  DrawRectangleRec(player->shape, RED);
+}
+
+void draw_player_bullets(Entity *player_bullets, int *player_bullet_count) {
+  for (int i = 0; i < *player_bullet_count; i++) {
+    DrawRectangleRec(player_bullets[i].shape, BLUE);
+  }
+}
+
+void draw_aliens(Entity *aliens, int *alive_alien_count) {
+  for (int i = 0; i < *alive_alien_count; i++) {
+    DrawRectangleRec(aliens[i].shape, GREEN);
+  }
 }
